558623b1dfcf3ebccb4e61afde521829f6898b54
Divahar23 <divaharbala@gmail.com>
2023-04-11 08:53:40 +0530

    Commited

---
+++ my-app/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Make a POST request to the backend for user authentication
+    try {
+      const response = await axios.post('http://localhost:3001', { username, password });
+      // Handle successful authentication
+      console.log('Authentication success:', response.data);
+    } catch (error) {
+      // Handle authentication error
+      setError('Invalid username or password');
+      console.error('Authentication error:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
+
